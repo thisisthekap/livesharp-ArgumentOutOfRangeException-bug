8437ad65e031c6f3656b238770452f4f9e7714c8
thisisthekap <c.kapplmueller@me.com>
2020-01-22 09:48:52 +0100

    initial commit

---
+++ test.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
